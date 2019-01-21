46d9ad74218a7a88be85c05a0b8781167ae585c9
Miguel Acevedo <migtomace@gmail.com>
2019-01-20 23:03:42 -0800

    Initial commit

---
+++ fh.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: Miguel.Acevedo
+ * Date: 1/20/2019
+ * Time: 5:44 PM
+ */
+
+//Name : <User entered data>
+//Email: <User entered data>
+//Phone : <User entered data, if no user entered data, do not show this line>
+//State: <User entered data, if no user entered data, do not show this line>
+
+$name = $_GET['name'];
+$email = $_GET['email'];
+$phone = $_GET['phone'];
+$state = $_GET['state'];
+
+echo "<ul>
+        <li>Name: ".$name."</li>
+        <li>Email: ".$email."</li>
+        <li>Phone: ".$phone."</li>
+        <li>State: ".$state."</li>
+    </ul>";
---
+++ main.js
@@ -0,0 +1,23 @@
+
+function validate() {
+    var bool = true;
+
+    var name = document.forms["form"]["name"].value;
+    var email = document.forms["form"]["email"].value;
+
+    if (name==null || name==""){
+        document.getElementById('nameReq').innerHTML = "*Required";
+        bool = false;
+    }
+    if (email==null || email==""){
+        document.getElementById('emailReq').innerHTML = "*Required";
+        bool = false;
+    }
+
+    return bool;
+}
+
+function clear() {
+    document.getElementById('nameReq').innerHTML = "";
+    document.getElementById('emailReq').innerHTML = "";
+}
